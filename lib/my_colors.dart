a8044c09da40c1094d9de7fc0b1d0baedc517ca3
Isadora Miranda <mirandacruzisadora@icloud.com>
2026-04-01 14:48:38 -0300

    Primeiro commit - projeto acervo

---
+++ lib/my_colors.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class MyColors {
+  // Construtor privado para evitar instanciação
+  MyColors._();
+
+//primarias
+  static const Color marrom = Color(0xFF2F1500);
+  static const Color marromMedio = Color (0xFF51230A);
+  static const Color marromClaro = Color(0xFF6E3900);
+  static const Color abobora = Color(0xFFB56100);
+  static const Color salmao = Color(0xFFFFB77D);
+  static const Color creme = Color( 0xFFFFEDE3);
+
+  // secundarias
+  static const Color preto= Color(0xFF000000);
+  static const Color azulPetroleo= Color(0xFF263046);
+  static const Color azulCinza= Color (0xFF545E76);
+  static const Color azulCeleste = Color(0xFFD7E2FF);
+  static const Color branco = Color(0xFFFFFFFF);
+
+//neutras
+  static const Color cinzaClaro = Color(0xFFEDF0FF);
+  static const Color cinzaNude = Color(0xFFCAC6BE);
+  static const Color cinzaMedio = Color(0xFF7A7770);
+  static const Color cinzaEscuro = Color(0xFF494740);
+
+
+}
