a92f9832bb6d77d0432f0d6c258a0071f76f09bd
Jordan Dourado Alves <jordan.douradoalves@gmail.com>
2026-04-01 21:49:32 +0000

    new file:   devtools_options.yaml 	modified:   lib/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
-import 'package:acervo/pages/home.page.dart';
 import 'package:flutter/material.dart';
-import 'package:supabase_flutter/supabase_flutter.dart'; //Importando o pacote do Supabase Flutter
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:acervo/pages/home.page.dart';
+import 'package:acervo/my_colors.dart';
 
-Future<void> main() async { //Função principal do aplicativo, marcada como assíncrona para permitir a inicialização do Supabase.
- 
-  WidgetsFlutterBinding.ensureInitialized(); //Garantindo que o Flutter esteja completamente inicializado antes de prosseguir.
-  await Supabase.initialize( //Inicializando o Supabase com as credenciais fornecidas.
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // 1. Conexão com o Banco de Dados Supabase
+  await Supabase.initialize(
     url: 'https://aocsoypewmyaulaagklz.supabase.co',
     anonKey: 'sb_publishable_xcExOS9YSNU5eBJGZxpdAQ_Q2OnibJy',
   );
@@ -15,70 +17,17 @@ Future<void> main() async { //Função principal do aplicativo, marcada como ass
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-  // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Acervo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: MyColors.marrom),
+        useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const HomePage(),
     );
   }
 }
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-   
-  }
-}
-
-
